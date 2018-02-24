e89f05feeb7322c62c888725ad8a59e8a7d9f708
Andrey Salnikov <a.salnikov.home@gmail.com>
2018-02-24 12:44:06 +0200

    Draw

--- cw-6-2/Program.cs
+++ cw-6-2/Program.cs
@@ -10,6 +10,34 @@ namespace cw_6_2
     {
         static void Main(string[] args)
         {
+            int curX = Console.WindowWidth / 2;
+            int curY = Console.WindowHeight / 2;
+            Console.SetCursorPosition(curX, curY);
+
+            while (true) 
+            {
+                ConsoleKeyInfo consoleKey = Console.ReadKey();
+
+                if (consoleKey.Key == ConsoleKey.UpArrow)
+                {
+                    Console.SetCursorPosition(curX, curY-=1);
+                }
+                            
+                else if (consoleKey.Key == ConsoleKey.DownArrow)
+                {
+                    Console.SetCursorPosition(curX, curY+=1);
+                }
+
+                else if (consoleKey.Key == ConsoleKey.RightArrow)
+                {
+                    Console.SetCursorPosition(curX+=1, curY);
+                }
+                else if (consoleKey.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.SetCursorPosition(curX-=1, curY);
+                }
+                Console.WriteLine('*');
+            }
         }
     }
 }
